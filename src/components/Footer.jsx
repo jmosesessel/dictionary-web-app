b7574538dcb813fc71fcc5eaf83b9d7934511117
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-09-05 11:42:36 -0400

    dark mode on searchbox

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer({isDarkMode}) {
   return (
     <>
-        <section className=' border-t-[1px] border-t-d-grey my-8 flex flex-col gap-2 pt-6 text-sm'>
+        <section className='border-t-[1px] border-t-d-grey my-8 flex flex-col lg:flex-row gap-2 pt-6 text-sm'>
             <h2 className="underline text-d-deep-grey">Source</h2>
             <a href="#" className={`${isDarkMode ? 'text-d-white' : ''}`}>https://en.wiktionary.org/wiki/keyboard</a>
         </section>
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,8 +4,8 @@ import SearchIcon from '../assets/search-icon.svg'
 function SearchBox({isDarkMode}) {
   return (
     <>
-        <section className={`${ isDarkMode ? 'bg-d-black' : 'bg-d-light-grey'}  w-full mb-6 flex justify-between gap-2 px-6 pt-[0.88rem] pb-[0.95rem] rounded-2xl`}>
-            <input className={`${ isDarkMode ? 'bg-d-black text-d-white' : 'bg-d-light-grey'}  w-[90%] font-[700] text-base border-none focus:outline-none focus:border-none focus-visible:ring-0`} type="text" />
+        <section className={`${ isDarkMode ? 'bg-d-lighter-black' : 'bg-d-light-grey'}  w-full mb-6 flex justify-between gap-2 px-6 pt-[0.88rem] pb-[0.95rem] rounded-2xl`}>
+            <input className={`${ isDarkMode ? ' bg-d-lighter-black text-d-white' : 'bg-d-light-grey'}  w-[90%] font-[700] text-base border-none focus:outline-none focus:border-none focus-visible:ring-0`} type="text" />
             <img src={SearchIcon} alt="search icon" />
         </section>
     </>
